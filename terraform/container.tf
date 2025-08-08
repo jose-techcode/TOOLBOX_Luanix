76aabe67e95edecdcf3b5f2f4bfbe533c3cd2c86
José <walterandrade0001@gmail.com>
2025-08-07 22:18:27 -0300

    Adição de terraform no projeto. Testes de aprendizaod.

---
+++ terraform/container.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+# Provider
+
+provider "docker" {}
+
+# Docker image
+
+resource "docker_image" "toolbox_luanix" {
+  name = "toolbox_luanix:latest"
+
+  build {
+    path = "~/Documentos/TOOLBOX_Luanix/" # full directory path
+    dockerfile = "Dockerfile"
+  }
+}
+
+# Docker container
+
+resource "docker_container" "toolbox_luanix" {
+  name  = "toolbox_luanix"
+  image = docker_image.toolbox_luanix.name
+
+  # Ports
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
